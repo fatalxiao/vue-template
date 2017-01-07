9cb9e1ab41b3282231839fae9fd74080b72a3ac4
fatalxiao@163.com <xiaomimi722>
2017-01-07 21:14:50 +0800

    update

--- src/containers/app/App.vue
+++ src/containers/app/App.vue
@@ -1,77 +1,76 @@
 <template>
-    <div class="app"
-         :class="{'collapsed': navMenuCollapsed}">
+	<div class="app"
+	     :class="{'collapsed': navMenuCollapsed}">
 
-        <NavMenu/>
+		<NavMenu/>
 
-        <div class="content-wrap"
-             @mousedown="contentMousedownHandle">
+		<div class="content-wrap"
+		     @mousedown="contentMousedownHandle">
 
-            <NavBar/>
+			<NavBar/>
 
-            <div class="content">
+			<div class="content">
 
-                <router-view class="content-router-view"></router-view>
+				<router-view class="content-router-view"></router-view>
 
-                <div class="copyright">
-                    Copyright &copy; 2016 Brand Co.,Ltd. All rights reserved.
-                </div>
+				<div class="copyright">
+					Copyright &copy; 2016-2017 Brand Co.,Ltd. All rights reserved.
+				</div>
 
-            </div>
+			</div>
 
-        </div>
+		</div>
 
-    </div>
+	</div>
 </template>
 
 <script type="text/ecmascript-6">
 
-    import {mapGetters, mapActions} from 'vuex';
+	import {mapGetters, mapActions} from 'vuex';
 
-    import NavBar from './navBar/NavBar';
-    import NavMenu from './menu/NavMenu';
+	import NavBar from './navBar/NavBar';
+	import NavMenu from './menu/NavMenu';
 
-    export default {
-        name: 'App',
-        components: {
-            NavBar,
-            NavMenu
-        },
-        computed: {
-            ...mapGetters({
-                isDesktop: 'isDesktop',
-                navMenuCollapsed: 'navMenuCollapsed'
-            })
-        },
-        methods: {
-            ...mapActions([
-                'collapseNavMenu',
-                'expandNavMenu'
-            ]),
-            contentMousedownHandle(e) {
-                // e.stopPropagation();
-                if (!this.isDesktop && !this.navMenuCollapsed) {
-                    this.collapseNavMenu();
-                }
-            }
-        },
-        created () {
+	export default {
+		name: 'App',
+		components: {
+			NavBar,
+			NavMenu
+		},
+		computed: {
+			...mapGetters({
+				isDesktop: 'isDesktop',
+				navMenuCollapsed: 'navMenuCollapsed'
+			})
+		},
+		methods: {
+			...mapActions([
+				'collapseNavMenu',
+				'expandNavMenu'
+			]),
+			contentMousedownHandle(e) {
+				// e.stopPropagation();
+				if (!this.isDesktop && !this.navMenuCollapsed) {
+					this.collapseNavMenu();
+				}
+			}
+		},
+		created () {
 
-            const navMenuCollapsed = localStorage.getItem('navMenuCollapsed');
+			const navMenuCollapsed = localStorage.getItem('navMenuCollapsed');
 
-            if (navMenuCollapsed == '1') {
-                this.collapseNavMenu();
-            } else if (navMenuCollapsed == '0') {
-                this.expandNavMenu();
-            }
+			if (navMenuCollapsed == '1') {
+				this.collapseNavMenu();
+			} else if (navMenuCollapsed == '0') {
+				this.expandNavMenu();
+			}
 
-        }
-    }
+		}
+	}
 
 </script>
 
 <style scoped>
 	@import '../../assets/stylesheets/containers/app/app.css';
-
 
 </style>
